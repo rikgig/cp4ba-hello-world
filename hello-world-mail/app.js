d1780d2cc1052322489c8fdfb4bba1dd9bbc599f
Remi Cauchon <REMI.CAUCHON@ibm.com>
2022-05-09 16:14:11 -0400

    adding new api email

---
+++ hello-world-mail/app.js
@@ -0,0 +1,89 @@
+// Imports
+var express = require("express");
+var myParser = require("body-parser");
+var app = express();
+require('dotenv').config();
+
+app.use(myParser.urlencoded({extended : true}));
+
+// Reference
+// https://github.com/sendgrid/sendgrid-nodejs/tree/main/packages/mail
+//
+
+const sgMail = require('@sendgrid/mail');
+console.log("ENVIRONMENT: "+ process.env.ENVIRONMENT);
+console.log("SENDGRID API_KEY: "+ process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.use(myParser.json());
+
+//const accountSid = process.env.TWILIO_ACCOUNT_SID;
+//const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = 'ACb47581bf98ec4c469062998aaf3cdb25';
+const authToken = '63362a85d6ae91b8a7495ff184111ade';
+const client = require('twilio')(accountSid, authToken);
+
+app.get("/sms", function(req, res) {
+
+  console.log("sending SMS");
+  client.messages
+  .create({
+     body: 'Message envoyer par mon Twilio SMS compte, Bonne journée Lucas',
+     from: '+13072882283',
+     to: '+15149735614'
+   })
+   .then(message => console.log(message.sid));
+
+  res.json({ name: "Sending sms to: hello" });
+});
+
+// Basic API /email
+app.post("/email", function (req, res) {
+
+  console.log(req.body);
+  if (!req.body.to) {
+    throw Error("to is required");
+  }
+  if (!req.body.from) {
+    throw Error("from is required");
+  }
+  if (!req.body.subject) {
+    throw Error("subject is required");
+  }
+  if (req.body.subject.length < 3) {
+    throw Error("subject should have at least 3 characters");
+  }
+  if (!req.body.html) {
+    throw Error("html is required");
+  }
+  if (req.body.html.length < 3) {
+    throw Error("html should have atleast 3 characters");
+  }
+    //ES6
+  var msg = {
+      to: req.body.to,
+      from: req.body.from,
+      subject: req.body.subject,
+      html: req.body.html
+    };
+
+
+  sgMail
+    .send(msg)
+    .then(() => {}, error => {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    });
+  
+  res.json({ name: "Sending email to: " + req.body.to });
+
+});
+
+
+// LISTEN ON PORT 8080
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
